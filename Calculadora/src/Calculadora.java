bd257d2e1727aa33b5261b8563b26eeb182e1c6c
João Vagner de Alencar Sampaio <6516648+VagnerSampaio@users.noreply.github.com>
2024-08-24 22:30:55 -0300

    javadoc

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * Classe de calculadora
+ * @author VagnerSampaio
+ * @version 1.0
+ * @since 24/08/2024
+ */
+public class Calculadora {
+
+    /**
+     * Metodo somar inteiros
+     * @param numeroUm
+     * @param numeroDois
+     * @return int
+     */
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+
+    //Gerar JavaDocs, colar no terminal
+    //javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+}
+
